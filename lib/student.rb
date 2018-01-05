7f84252c851eb2de4ef12eb711f177a920776c9d
ronald vilorio <soccer23415@gmail.com>
2018-01-05 00:17:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,8 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def initialize(name=nil,grade=nil)
-
+    @name = name
+    @grade = grades
   end
 
 end
