a77d9637841ceda1b33e456d55296a4c919339ee
ronald vilorio <soccer23415@gmail.com>
2018-01-05 00:30:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,7 @@
 class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  attr_accessor :name,:grade
+  attr_accessor :name,:grade,:id
   DB = {:conn=>SQLite3::Database.new("db/students.db")}
 
   def initialize(name=nil,grade=nil,id=nil)
